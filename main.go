dea5789bd521b7243c856d43283b68790ee91ba9
estshorter <1430311+estshorter@users.noreply.github.com>
2020-04-29 17:55:10 +0900

    Call keyboard.Close() only once

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func timeout(waitSecond int) {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 	done := make(chan struct{})
-	timeout := time.Now().Add(time.Second * time.Duration(waitSecond))
+	timeoutTime := time.Now().Add(time.Second * time.Duration(waitSecond))
 
 	var once sync.Once
 	go func() {
@@ -31,21 +31,20 @@ func timeout(waitSecond int) {
 	}()
 
 	go func() {
-		_, _, err := keyboard.GetSingleKey()
-		if err != nil {
-			panic(err)
+		if err := keyboard.Open(); err == nil {
+			keyboard.GetKey()
 		}
 		once.Do(func() { done <- struct{}{} })
 	}()
+	defer keyboard.Close()
 
 	for {
 		select {
 		case <-done:
 			fmt.Println("")
-			keyboard.Close()
 			return
 		case t := <-ticker.C:
-			fmt.Printf("\rWaiting for %v seconds, press any key to quit...", timeout.Sub(t).Round(time.Second).Seconds())
+			fmt.Printf("\rWaiting for %v seconds, press any key to quit...", timeoutTime.Sub(t).Round(time.Second).Seconds())
 		}
 	}
 }
